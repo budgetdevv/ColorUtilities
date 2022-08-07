a95dc10980eb67820b592a3ce84e83efaab9391e
BudgetDevv <budgetdevv@gmail.com>
2022-08-07 14:22:13 +0800

    Added Optimizer.cs

---
+++ ColorEncoding/Optimizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Color.Encoding
+{
+    public static class Optimizer
+    {
+        [ModuleInitializer]
+        internal static void Optimize()
+        {
+            foreach (var Type in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                RuntimeHelpers.RunClassConstructor(Type.TypeHandle);
+            }
+        }
+    }
+}
